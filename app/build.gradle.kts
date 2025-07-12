b41f6368f75cf3d25d3a935560f7d12d04cdc34b
EmmanuelBetancur <ebetancuru@unal.edu.co>
2025-07-12 12:55:46 -0500

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.shivaishta.prueba20"
-        minSdk = 25
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
