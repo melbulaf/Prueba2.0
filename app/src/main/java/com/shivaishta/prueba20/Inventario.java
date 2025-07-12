007dcc66496d130c9dc3f55f06d19d38524d0b57
EmmanuelBetancur <ebetancuru@unal.edu.co>
2025-07-12 13:17:00 -0500

    Commit

---
+++ app/src/main/java/com/shivaishta/prueba20/Inventario.java
@@ -0,0 +1,28 @@
+package com.shivaishta.prueba20;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Inventario implements Serializable {
+
+    public static ArrayList<Producto> productos;
+    public static Inventario instancia;
+    public Inventario() {
+        productos = new ArrayList<>();
+    }
+    public static Inventario getInstancia() {
+        if (instancia == null) {
+            instancia = new Inventario();
+        }
+        return instancia;
+    }
+
+    public ArrayList<Producto> obtenerProductos() {
+        return productos;
+    }
+
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+}
