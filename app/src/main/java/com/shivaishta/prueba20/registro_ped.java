1404d811447bf4a07e4033d61e17ca20344a0272
EmmanuelBetancur <ebetancuru@unal.edu.co>
2025-07-14 11:12:14 -0500

    Registro Pedido

--- app/src/main/java/com/shivaishta/prueba20/registro_ped.java
+++ app/src/main/java/com/shivaishta/prueba20/registro_ped.java
@@ -1,6 +1,10 @@
 package com.shivaishta.prueba20;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,29 @@ public class registro_ped extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Boton agregar producto
+        TableLayout tabla = findViewById(R.id.tablaProductos);
+
+        TableRow fila = new TableRow(this);
+        TextView producto = new TextView(this);
+        producto.setText("Producto A");
+        TextView cantidad = new TextView(this);
+        cantidad.setText("3");
+
+        // Botón eliminar
+        Button btnEliminar = new Button(this);
+        btnEliminar.setText("X");
+        btnEliminar.setOnClickListener(v -> tabla.removeView(fila));
+
+        // Agregar a la fila
+        fila.addView(producto);
+        fila.addView(cantidad);
+        fila.addView(btnEliminar);
+
+        // Agregar a la tabla
+        tabla.addView(fila);
+
+
     }
 }
